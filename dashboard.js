a7e303f479d804088960dc0e51dabbd3290f988b
Vanshika Sharma <71334340+Vanshikaweb@users.noreply.github.com>
2023-03-25 11:16:25 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,26 +1,18 @@
-const allusers = document.getElementById("allusers");
-fetch("http://3.229.255.54:3000/api/users", {
-  method: "GET",
-  // body: JSON.stringify({ name:fullName,guid:userName, phone, email, password }),
-  headers: { "Content-Type": "application/json" },
-})
-  .then((response) => response.json())
-  .then((response) => {
-    console.log(response);
-    // const fresponse = await response.json();
-    for (let i = 0; i < response.length; i++) {
-      console.log(response[i]);
-      const msg = `<h1>username: ${response[i].guid}</h1>
-        <h2>name: ${response[i].name}</h2>
-        <h2>phone: ${response[i].phone}</h2>
-        <h2>email: ${response[i].email}</h2>`;
-      var item = document.createElement("li");
-      item.innerHTML = msg;
-      allusers.appendChild(item);
-      window.scrollTo(0, document.body.scrollHeight);
-      // const ele= document.createElement('li')
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const userId = urlParams.get("id");
+function redirecttoprofile() {
+
+ return window.location.href =`profile.html?id=${userId}`;
+}
+const logoutBtn = document.getElementById("logout-btn");
+
+// Add a click event listener to the logout button
+logoutBtn.addEventListener("click", () => {
+  // Clear the user data from local storage
+  localStorage.removeItem("user");
+
+  // Redirect the user to the login page
+  window.location.href = "index.html";
+});
+
