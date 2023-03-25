9d53cae3c5d0e61783b144bc8d9b9abf36311d2a
Vanshika Sharma <71334340+Vanshikaweb@users.noreply.github.com>
2023-03-25 11:17:05 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,41 @@
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const userId = urlParams.get("id");
+
+const allusers = document.getElementById("allusers");
+
+// const userId= document.getElementById("users-id"); // Change this to the username of the successfully registered user
+
+fetch(`http://54.83.240.126:3000/api/users/${userId}`, {
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
+})
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+
+    const user = response; // Get the first user from the filtered response
+    const msg = `<h1>username: ${user.guid}</h1>
+        <h2>name: ${user.name}</h2>
+        <h2>phone: ${user.phone}</h2>
+        <h2>email: ${user.email}</h2>`;
+    var item = document.createElement("li");
+    item.innerHTML = msg;
+    allusers.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+// Get the logout button
+const logoutBtn = document.getElementById("logout-btn");
+
+// Add a click event listener to the logout button
+logoutBtn.addEventListener("click", () => {
+  // Clear the user data from local storage
+  localStorage.removeItem("user");
+
+  // Redirect the user to the login page
+  window.location.href = "index.html";
+});
