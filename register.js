5acfe5ebeff5919e735fcab34985a8f045c84238
Vanshika Sharma <71334340+Vanshikaweb@users.noreply.github.com>
2023-03-25 11:15:39 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,3 +1,6 @@
+
+
+
 const registrationForm = document.getElementById("registration-form");
 
 registrationForm.addEventListener("submit", (event) => {
@@ -16,16 +19,19 @@ registrationForm.addEventListener("submit", (event) => {
     return;
   }
 
-  fetch("http://3.229.255.54:3000/api/users", {
+  fetch("http://54.83.240.126:3000/api/users", {
     method: "POST",
     body: JSON.stringify({ name:fullName,guid:userName, phone, email, password }),
     headers: { "Content-Type": "application/json" },
   })
-    .then((response) => {
-      console.log(response.json())
-      if (response.ok) {
-        window.location.href = "dashboard.html";
-      } else {
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    if (response) {
+    
+      window.location.href = "/index.html";
+    }
+      else {
         console.error("Registration failed.");
       }
     })
@@ -33,6 +39,3 @@ registrationForm.addEventListener("submit", (event) => {
       console.error(error);
     });
 });
-
-
-
